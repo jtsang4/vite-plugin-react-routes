705461f1082a9ea7ee48a12524f7416865077790
jtsang4 <wtzeng1@gmail.com>
2022-02-08 13:28:09 +0800

    chore: remove unused files

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import jiti from 'jiti'
-
-const filename = jiti(process.cwd(), {
-  extensions: ['.js', '.mjs', '.cjs', '.ts', '.tsx', '.jsx'],
-}).resolve('./layout')
-
-console.log('name:', filename)
